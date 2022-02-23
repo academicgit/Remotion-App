3040d767c68fd8df3acf3b8c17b0a634f4f8ed6a
Parv Ashwani <hackyourworldwithparv@gmail.com>
2022-02-24 00:54:02 +0530

    Updates

---
+++ precache-manifest.250df5951752e83f5e85488af5b7e7c1.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "949eea98a6b3c840eef8876ec67897fa",
+    "url": "/Remotion-App/index.html"
+  },
+  {
+    "revision": "f2c1146e41450caa9067",
+    "url": "/Remotion-App/static/css/main.94396172.chunk.css"
+  },
+  {
+    "revision": "5da4ed6cb453c2fa77e7",
+    "url": "/Remotion-App/static/js/2.4761b77b.chunk.js"
+  },
+  {
+    "revision": "f2c1146e41450caa9067",
+    "url": "/Remotion-App/static/js/main.71010116.chunk.js"
+  },
+  {
+    "revision": "5bee4bd7c948e44243e1",
+    "url": "/Remotion-App/static/js/runtime~main.663979f5.js"
+  }
+]);
